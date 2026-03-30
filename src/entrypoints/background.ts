4f9ed3892a1ecb9bb64589eba6ef952463e74dcc
Edward Ji <jiziao6@gmail.com>
2026-03-30 23:36:21 +1100

    feat: support Microsoft Edge

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,11 +1,11 @@
 export default defineBackground(() => {
-  if (import.meta.env.BROWSER === 'chrome') {
+  if (import.meta.env.BROWSER === 'chrome' || import.meta.env.BROWSER === 'edge') {
     // Allows users to open the side panel by clicking on the action toolbar icon
     chrome.sidePanel
       .setPanelBehavior({ openPanelOnActionClick: true })
       .catch((error) => console.error(error));
-  } else {
-    // Firefox: open sidebar when toolbar icon is clicked
+  } else if (import.meta.env.BROWSER === 'firefox') {
+    // Open sidebar when toolbar icon is clicked
     browser.action.onClicked.addListener(() => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (browser as any).sidebarAction.open().catch((error: unknown) => console.error(error));
