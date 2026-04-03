322d41025ec49c3a6a8318cfb5b78e8a9b03448a
Edward Ji <jiziao6@gmail.com>
2026-04-04 10:04:38 +1100

    feat(worker): add redirect for Microsoft Edge

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,26 +2,26 @@
  * Test Cards Extension Redirect Worker
  */
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Env { }
-
 export default {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request): Promise<Response> {
 		const userAgent = request.headers.get('User-Agent') || '';
 
 		const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-		const isChromeDesktop = /Chrome/i.test(userAgent) && !/Edge|Edg|OPR|Opera/i.test(userAgent) && !isMobile;
+		const isChromeDesktop = /Chrome/i.test(userAgent) && !/(Edge|Edg|OPR|Opera)/i.test(userAgent) && !isMobile;
 		const isFirefoxDesktop = /Firefox/i.test(userAgent) && !isMobile;
+		const isEdgeDesktop = /(Edg|Edge)\//i.test(userAgent) && !isMobile;
 
 		const chromeUrl = 'https://chromewebstore.google.com/detail/ddflppoejkafcaedakefoakkmaholoeh';
 		const firefoxUrl = 'https://addons.mozilla.org/en-US/firefox/addon/test-cards/';
+		const edgeUrl = 'https://microsoftedge.microsoft.com/addons/detail/test-cards/cfejdbkkfbpjdfafkmjcpnkgmdegcdjl';
 		const githubIssueUrl = 'https://github.com/edward-ji/test-cards-extension/issues/1';
 
 		if (isChromeDesktop) {
 			return Response.redirect(chromeUrl, 302);
 		} else if (isFirefoxDesktop) {
 			return Response.redirect(firefoxUrl, 302);
+		} else if (isEdgeDesktop) {
+			return Response.redirect(edgeUrl, 302);
 		} else {
 			return Response.redirect(githubIssueUrl, 302);
 		}
